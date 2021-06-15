81b28cbf4e1c8cb63cb7e6cd9473edc30ebc9eaf
Angelia Marceline <angelia.marceline@bankmandiri.co.id>
2021-06-15 22:59:55 +0700

    sidebar js

--- js/sidebar.js
+++ js/sidebar.js
@@ -1,4 +1,4 @@
-/* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
+/* Set the width of the sidebar to 347px and the left margin of the page content to 347px */
 function openNav() {
     document.getElementById("mySidebar").style.width = "347px";
     document.getElementById("main").style.marginLeft = "347px";
